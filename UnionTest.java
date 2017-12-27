36efa8e4bdca23cb9fa488612396a4e994f1c243
ChristianBermejoGuerrero <cbermejog7@gmail.com>
2017-12-27 16:25:58 +0100

    UnionTest before y after

--- UnionTest.java
+++ UnionTest.java
@@ -4,7 +4,22 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
 
+import union.Union;
+
 public class UnionTest
 {
+	private Vector a;
+	private Vector b;
 
+	@Before // Set up - Called before every test method
+	public void setUp(){
+		a = new Vector();
+		b = new Vector();
+	}
+
+	@After // Tead down - Called after every test method
+	public void tearDown(){
+		a = null;
+		b = null;
+	}
 }
